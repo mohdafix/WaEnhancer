82357951e74044a257169169e4f70db495c045f6
MohdAfix <linexsg@gmail.com>
2026-01-27 23:16:05 +0800

    Fix background blur on BottomDialogWpp by correctly identifying Activity context

--- app/src/main/java/com/wmods/wppenhacer/views/dialog/BottomDialogWpp.java
+++ app/src/main/java/com/wmods/wppenhacer/views/dialog/BottomDialogWpp.java
@@ -1,6 +1,8 @@
 package com.wmods.wppenhacer.views.dialog;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -17,10 +19,20 @@ import com.wmods.wppenhacer.xposed.utils.Utils;
 public class BottomDialogWpp {
 
     private final Dialog dialog;
+    private Activity mActivity;
     private boolean blurEnabled = false;
 
     public BottomDialogWpp(@NonNull Dialog dialog) {
         this.dialog = dialog;
+        Context context = dialog.getContext();
+        if (context instanceof Activity) {
+            this.mActivity = (Activity) context;
+        } else if (context instanceof android.content.ContextWrapper) {
+            Context baseContext = ((android.content.ContextWrapper) context).getBaseContext();
+            if (baseContext instanceof Activity) {
+                this.mActivity = (Activity) baseContext;
+            }
+        }
     }
 
     public void dismissDialog() {
@@ -28,8 +40,11 @@ public class BottomDialogWpp {
     }
 
     public void showDialog() {
-        if (blurEnabled && dialog.getOwnerActivity() != null) {
-            final var activity = dialog.getOwnerActivity();
+        Activity activity = mActivity;
+        if (activity == null) activity = dialog.getOwnerActivity();
+        
+        if (blurEnabled && activity != null) {
+            final var finalActivity = activity;
             if (android.os.Build.VERSION.SDK_INT >= 31) {
                 try {
                     activity.getWindow().getDecorView().setRenderEffect(RenderEffect.createBlurEffect(20f, 20f, Shader.TileMode.CLAMP));
@@ -39,15 +54,15 @@ public class BottomDialogWpp {
                     View decorView = activity.getWindow().getDecorView();
                     Bitmap bitmap = Bitmap.createBitmap(decorView.getWidth(), decorView.getHeight(), Bitmap.Config.ARGB_8888);
                     decorView.draw(new Canvas(bitmap));
-                    Bitmap blurred = Utils.blurBitmap(activity, bitmap, 25f);
-                    dialog.getWindow().setBackgroundDrawable(new BitmapDrawable(activity.getResources(), blurred));
+                    Bitmap blurred = Utils.blurBitmap(finalActivity, bitmap, 25f);
+                    dialog.getWindow().setBackgroundDrawable(new BitmapDrawable(finalActivity.getResources(), blurred));
                 } catch (Exception ignored) {}
             }
 
             dialog.setOnDismissListener(d -> {
                 if (android.os.Build.VERSION.SDK_INT >= 31) {
                     try {
-                        activity.getWindow().getDecorView().setRenderEffect(null);
+                        finalActivity.getWindow().getDecorView().setRenderEffect(null);
                     } catch (Exception ignored) {}
                 }
             });
